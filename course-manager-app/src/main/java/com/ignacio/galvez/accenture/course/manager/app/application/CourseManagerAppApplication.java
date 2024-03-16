c17ec742b989e8933bff89b0fdbef1d8b952b807
iggalvez2023 <iggalvez2023@gmail.com>
2024-03-16 19:18:35 -0300

    - Adding Course and it's repository.  - Testing Course creation.

---
+++ course-manager-app/src/main/java/com/ignacio/galvez/accenture/course/manager/app/application/CourseManagerAppApplication.java
@@ -0,0 +1,21 @@
+package com.ignacio.galvez.accenture.course.manager.app.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(CourseManagerAppApplication.PACKAGES)
+@ComponentScan(basePackages = { CourseManagerAppApplication.PACKAGES })
+@EntityScan(CourseManagerAppApplication.PACKAGES)
+@SpringBootApplication
+public class CourseManagerAppApplication {
+
+	public static final String PACKAGES = "com.ignacio.galvez.accenture.course.manager.app.*";
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagerAppApplication.class, args);
+	}
+
+}
