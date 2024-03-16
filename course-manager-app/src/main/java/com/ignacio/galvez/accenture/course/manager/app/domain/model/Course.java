a5095743b25586c43d22a9894d0ec81e79bdb2cd
iggalvez2023 <iggalvez2023@gmail.com>
2024-03-16 19:21:14 -0300

    - Added Course id test passing.

--- course-manager-app/src/main/java/com/ignacio/galvez/accenture/course/manager/app/domain/model/Course.java
+++ course-manager-app/src/main/java/com/ignacio/galvez/accenture/course/manager/app/domain/model/Course.java
@@ -2,8 +2,13 @@ package com.ignacio.galvez.accenture.course.manager.app.domain.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
 
+import java.sql.Types;
 import java.util.UUID;
 
 @Entity
@@ -11,5 +16,8 @@ import java.util.UUID;
 public class Course {
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JdbcTypeCode(value = Types.VARCHAR)
     private UUID id;
 }
